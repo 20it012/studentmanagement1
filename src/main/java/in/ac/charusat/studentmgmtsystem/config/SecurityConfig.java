3df715096b76c3f2f2de7e9725a5bc7e81e5ef19
divyesh <20it012@charusat.edu.in>
2022-09-13 12:11:26 +0530

    cross origin added

--- src/main/java/in/ac/charusat/studentmgmtsystem/config/SecurityConfig.java
+++ src/main/java/in/ac/charusat/studentmgmtsystem/config/SecurityConfig.java
@@ -13,7 +13,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("20it012")
+                .withUser("ADMIN")
                 .password("ADMIN")
                 .roles("ADMIN")
                 .and()
