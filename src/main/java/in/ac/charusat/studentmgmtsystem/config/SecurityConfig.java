2905e61585403c56897249c8599b543007d75c08
divyesh <20it012@charusat.edu.in>
2022-09-13 12:17:05 +0530

    security user added multiple user

--- src/main/java/in/ac/charusat/studentmgmtsystem/config/SecurityConfig.java
+++ src/main/java/in/ac/charusat/studentmgmtsystem/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .roles("ADMIN")
                 .and()
                 .withUser("USER")
-                .password("uSER")
+                .password("usER")
                 .roles("USER");
     }
 
